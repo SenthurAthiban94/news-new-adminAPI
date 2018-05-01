8b43455130133b2745827a15acd650910550d36c
senthur <senthur@financebuddha.com>
2018-05-01 13:02:01 +0530

    API done for multiple insertions of sites and cron file is completed

--- api/controllers/apiController.js
+++ api/controllers/apiController.js
@@ -12,17 +12,52 @@ exports.list_all_Sites = function(req, res) {
 };
 
 
-
-
 exports.create_a_Site = function(req, res) {
-  var new_site = new Sites(req.body);
-  new_site.save(function(err, site) {
-    if (err)
-      res.send(err);
-    res.json(site);
-  });
+  var multipleinserts=(Object.values(req.body).filter(a=> typeof(a) == 'object').length > 0) ? true : false;
+  if(!multipleinserts){
+    var new_site = new Sites(req.body);
+        new_site.save(function(err, site) {
+            if (err)
+            res.send(err);
+            res.json({status : 1,result: site});
+        });
+  }
+  else{
+    var payload=req.body,
+        response=[];
+        payload.map(e => {
+          var new_site= new Sites(e);
+              new_site.save(function(err,site){
+                if(err){
+                  res.send(err);
+                }else{
+                  response.push(site);
+                }
+                if(response.length==payload.length){
+                  res.json({status : 1,result:response});
+                }
+              });
+        });
+ }
 };
 
+
+function SaveAll(payload){
+  var response=Object.keys(payload).filter((e,i)=>{
+      var new_site = new Sites(payload[e]);
+      new_site.save(function(err, site) {
+          if (err){
+            res.send(err);
+          }
+          else{
+            // response.push(site);
+            return site;
+          }
+      });
+  });
+  console.log
+  return response;
+}
 
 exports.read_a_Site = function(req, res) {
     Sites.findById(req.params.siteId, function(err, site) {
---
+++ cronTab/cron.js
@@ -0,0 +1,37 @@
+var trends = require('node-google-search-trends');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function getTOP_IndiaSites(){
+    trends('India', 10, function(err, data) {
+        if (err) return console.err(err);
+        // console.log(JSON.stringify(data, null, 2));  // Pretty prints JSON 'data'
+        let topSites=[];
+        Object.keys(data).map((e)=>{
+        var singlesite={};
+            singlesite.title=(data[e]['title'].length) ? data[e]['title'][0] : "";
+            singlesite.traffic=data[e]['ht:approx_traffic'].length ? data[e]['ht:approx_traffic'][0].replace(/,/g,'').replace('+','') : "";
+            singlesite.description=data[e]['description'].length ? data[e]['description'][0] : "";
+            singlesite.contentLink=data[e]['ht:news_item'].length ? ((data[e]['ht:news_item'][0]['ht:news_item_url'].length) ? data[e]['ht:news_item'][0]['ht:news_item_url'][0] : "" ) : "";
+            singlesite.contentImageUrl=data[e]['ht:picture'].length ? data[e]['ht:picture'][0] : "";
+            singlesite.dateTracked=data[e]['pubDate'].length ? data[e]['pubDate'][0] : "";
+            topSites.push(singlesite);
+        });
+
+        // Sending and receiving data in JSON format using POST method
+        //
+        var xhr = new XMLHttpRequest();
+        var url = "http://localhost:3000/sites";            //https://adminsa.herokuapp.com/sites
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var json = JSON.parse(xhr.responseText);
+                console.log(json);
+            }
+        };
+        var data = JSON.stringify(topSites);
+        xhr.send(data);
+    });
+}
+
+getTOP_IndiaSites();
